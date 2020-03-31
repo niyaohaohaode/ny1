8d472a82349dcba7568e937127e91b6e263b9cae
niyaohaohaode <45806475+niyaohaohaode@users.noreply.github.com>
2020-03-31 17:25:06 +0800

    ny1
    ny1

---
+++ style/js/index.js
@@ -0,0 +1,18 @@
+$(function() {
+	$('.slideNavBtn').click(function() {
+		if ($('#navBtn_control').is(':hidden')) {
+			$('#navBtn_control').show(); 
+		} else { 
+			$('#navBtn_control').hide();
+		}
+	})
+})
+$(document).ready(function() {
+	$('.flexslider').flexslider({
+		directionNav: true,
+		pauseOnAction: false,
+		animationSpeed: 300,
+		slideshowSpeed: 3000,
+		animation: "fade"
+	});
+});
